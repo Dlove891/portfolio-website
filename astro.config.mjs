6a8b51efe21f39509e883d6793141c5ac94a61c0
Dylan Love <djlove@berkeley.edu>
2025-07-09 16:06:05 -0700

    switched names

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
-  base: '/portfolio-website', // <- replace with actual repo name
+  base: '/my-portfolio-ws', // <- replace with actual repo name
   vite: {
     plugins: [tailwindcss()]
   }
