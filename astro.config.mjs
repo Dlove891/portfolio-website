2d53ed04121e718132456d1aadb2fc3697b0ac5d
Dylan Love <djlove@berkeley.edu>
2025-07-09 17:06:01 -0700

    sksks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind'; // ✅ correct import
 
 export default defineConfig({
-  output: 'static',
   site: 'https://dlove891.github.io',
   base: '/portfolio-website', // ✅ set the base path for your project-website
   integrations: [tailwind()], // ✅ use Astro's integration
